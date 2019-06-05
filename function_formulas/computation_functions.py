356981a13214356c19e0c7d28c097b400a64c68f
Allen Tools <allenchristophertools@gmail.com>
2019-06-05 14:38:40 -0400

    Add crit_temp feature to calculations

--- function_formulas/computation_functions.py
+++ function_formulas/computation_functions.py
@@ -8,6 +8,7 @@ Calculation functions:
 	common_areas() print computations of some common areas
 	gaussian_function() computes the values for gaussian function
 	air_resistance() prints the drag force and force gravity
+        critical_temp() computes critical temperature from heating an object
 
 
 """
@@ -181,5 +182,47 @@ def air_resistance(g=9.81, q=1.2, a=11, m=0.43, velocity=120):
 
     print("ratio of drag force=%.1f and force gravity=%.1f: %.1f" % \
           (Fd_high_impact, Fg, float(Fd_high_impact/Fg)))
+
+def critical_temp(init_temp=4, final_temp=70, water_temp=100,
+                  mass=47, density=1.038, heat_capacity=3.7,
+                  thermal_conductivity=5.4*10**-3):
+    """
+    Heating to a temperature with prevention to exceeding critical
+    points. Be defining critial temperature points based on
+    composition, e.g., 63 degrees celcius outter and 70 degrees
+    celcius inner we can express temperature and time as a
+    function.
+
+
+    Calculates the time for the center critical temp as a function
+        of temperature of applied heat where exceeding passes a critical point.
+
+    t = (M**(2/3)*c*rho**(1/3)/(K*pi**2*(4*pi/3)**(2/3)))*(ln(0.76*((To-Tw)/(Ty-Tw))))
+
+    Arguments:
+        init_temp: initial temperature in C of object e.g., 4, 20
+        final_temp: desired temperature in C of object e.g., 70
+        water_temp: temp in C for boiling water as a conductive fluid e.g., 100
+        mass: Mass in grams of an object, e.g., small: 47, large: 67
+        density: rho in g cm**-3 of the object e.g., 1.038
+        heat_capacity: c in J g**-1 K-1 e.g., 3.7
+        thermal_conductivity: in W cm**-1 K**-1 e.g., 5.4*10**-3
+    Returns: Time as a float in seconds to reach temperature Ty.
+    """
+    from sympy import symbols
+    from sympy import lambdify
+    from sympy import sympify
+    from numpy import pi
+    from numpy import log as ln # using ln
+
+    # using non-pythonic math notation create variables
+    M, c, rho, K, To, Tw, Ty = symbols("M c rho K To Tw Ty")
+    # writing out the formula
+    t = sympify('(M**(2/3)*c*rho**(1/3)/(K*pi**2*(4*pi/3)**(2/3)))*(ln(0.76*((To-Tw)/(Ty-Tw))))')
+    # using symbolic formula representation to create a function
+    time_for_Ty = lambdify([M, c, rho, K, To, Tw, Ty], t)
+    # return the computed value
+    return time_for_Ty(M=mass, c=heat_capacity, rho=density, K=thermal_conductivity,
+                       To=init_temp, Tw=water_temp, Ty=final_temp)
 
 
