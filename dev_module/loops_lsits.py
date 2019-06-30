09cb0116bc5cb7245da5de4274dbc3fe3206d20c
Al Tools <email@allen.tools>
2019-06-29 21:59:46 -0400

    Update with electron energy formula

--- dev_module/loops_lsits.py
+++ dev_module/loops_lsits.py
@@ -83,13 +83,96 @@ def odds(n=9):
 
     return None
 
-def _function():
-    """Computes sums of first n integers.
+def formula():
+    """Helper function for compute_atomic_energy
 
-    Computes the sum of integers from 1 to n, (inclusive), n(n + 1)/2
+    Returns a formula to be called.
     """
 
+    # Symbolic computing
+    from sympy import (
+        symbols,  # define symbols for symbolic math
+        lambdify,  # turn symbolic expr. into python functions
+    )
+
+    # declare symbolic variables
+    m_e, e, epsilon_0, h, n = symbols('m_e e epsilon_0 h n')
+    # formula
+    En = -(m_e*e**4)/(8*epsilon_0*h**2)*(1/n**2)
+
+    # convert to python function
+    return lambdify([m_e, e, epsilon_0, h, n],  # arguments in En
+                    En)  # symbolic expression
+
+def compute_atomic_energy(m_e=9.094E-34,
+                          e=1.6022E-19,
+                          epsilon_0=9.9542E-12,
+                          h=6.6261E-34):
+    """Compute energy levels in an atom
+
+    Computes the n-th energy level for an electron in an atom, e.g.,
+    Hydrogen: En = -(m_e*e**4)/(8*epsilon_0*h**2)*(1/n**2)
+        where:
+            m_e=9.094E-34 kg is the electron mass
+            e=1.6022E-19C is the elementary charge
+            epsilon_0=9.9542E-12 s^2 kg^{-1} m^{-3} is electrical permittivity of vacuum
+            h=6.6261E-34Js heat/energy
+
+        Calculates energy level En for n= 1,...,20
+    """
+
+
+    En = 0 # energy level of an atom
+    for n in range(1, 20): # Compute for 1,...,20
+        En += formula()(m_e, e, epsilon_0, h, n)
+
+    return En
+
+# Symbolic computing
+from sympy import (
+    symbols,  # define symbols for symbolic math
+    lambdify,  # turn symbolic expr. into python functions
+    )
+
+# declare symbolic variables
+m_e, e, epsilon_0, h, ni, nf = symbols('m_e e epsilon_0 h ni nf')
+# formula
+delta_E = -(m_e*e**4)/(8*epsilon_0*h**2)*((1/ni**2)-(1/nf**2))
+
+# convert to python function
+y = lambdify([m_e, e, epsilon_0, h, ni, nf],  # arguments in En
+             delta_E)  # symbolic expression
+
+def compute_change_in_energy(m_e=9.094E-34,
+                             e=1.6022E-19,
+                             epsilon_0=9.9542E-12,
+                             h=6.6261E-34):
+    """Creates a table with energy released by level and total energy.
+
+    A display function for compute_atomic_energy.
+    """
+
+    print("Energy released going from level to level.")
+    En = y(m_e, e, epsilon_0, h, 2, 1) # energy at level 1
+    for n in range(2, 20): # Compute for 1,...,20
+        En += y(m_e, e, epsilon_0, h, n-1, n)
+        print("{:23.2E}  {:7} to level {:2}".format(
+            y(m_e, e, epsilon_0, h, n-1, n),
+            n-1,
+            n))
+    print("Total energy: {:.2E}".format(compute_atomic_energy()))
+
+
+def _function():
+    """Docstring Summary
+
+    Doc String
+    """
+
+
     return None
+
+
 
 if __name__ == "__main__":
     _function()
